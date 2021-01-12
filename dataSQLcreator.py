954395ec8f33eef32bf87bb9d86ad8e38bb0113d
Arnaud VDR <52168487+arnaud-van-den-reysen@users.noreply.github.com>
2021-01-12 16:32:35 +0100

    Ajout des bornes

--- dataSQLcreator.py
+++ dataSQLcreator.py
@@ -21,11 +21,24 @@ for i in data:
             "INSERT INTO station (IDStation, Latitude, Longitude, Adresse, Payement, Horaire) VALUES (\'" + str(i['ID']) + "\', \'" + str(i['AddressInfo']['Latitude']) + "\', \'" + str(i['AddressInfo']['Longitude']) + "\', \'" + str(i['AddressInfo']['AddressLine1']) + "\', \'" + str(i['UsageTypeID']) + "\', \'24/7\');"
         )
     except:
+        print("ERROR Station")
         stationData.append(
             "INSERT INTO station (IDStation, Latitude, Longitude, Adresse, Payement, Horaire) VALUES (\'" + str(i['ID']) + "\', \'" + str(i['AddressInfo']['Latitude']) + "\', \'" + str(i['AddressInfo']['Longitude']) + "\', \'" + str(i['AddressInfo']['AddressLine1']) + "\', \'" + str(-1) + "\', \'24/7\');"
         )
 
-print(stationData)
+for i in data:
+    for j in i['Connections']:
+        try:
+            borneData.append(
+                "INSERT INTO station (IDBorne, IDStation, IDConnecteur, Puissance, Status) VALUES (\'" + str(j['ID']) + "\', \'" + str(i['ID']) + "\', \'" + str(j['ConnectionTypeID']) + "\', \'" + str(j['PowerKW']) + "\', \'" + str(j['StatusTypeID']) + "\');"
+            )
+        except:
+            print("ERROR Borne")
+            borneData.append(
+                "INSERT INTO station (IDBorne, IDStation, IDConnecteur, Puissance, Status) VALUES (\'" + str(j['ID']) + "\', \'" + str(i['ID']) + "\', \'" + str(j['ConnectionTypeID']) + "\', \'" + str(-1) + "\', \'" + str(-1) + "\');"
+            )
+
+print(borneData[56])
 
 fs = open("station.sql","w",encoding="utf-8")
 
@@ -34,3 +47,10 @@ for i in stationData:
     fs.write('\n%s' % i)
 
 fs.close()
+
+fb = open("borne.sql","w",encoding="utf-8")
+
+for i in borneData:
+    fb.write('\n%s' % i)
+
+fb.close()
